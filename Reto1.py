cd00268028ad4d1367506e3ad7cabcdc1c1f35de
ArnovisGT <ar.5gtreboll@gmail.com>
2022-05-14 21:00:45 -0500

    comnetario

--- Reto1.py
+++ Reto1.py
@@ -11,3 +11,4 @@ def CDT(usuario:str, capital:int, tiempo:int):
         return f"Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {valor_Total}"
 print(CDT('AB1012', 1000000, 3))
 print(CDT('ER3366',650000, 2))
+#reto1 completed 
