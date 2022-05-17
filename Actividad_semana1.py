7eb09d63130596f94094ec4819a5d687d83432e2
ArnovisGT <ar.5gtreboll@gmail.com>
2022-05-16 21:48:01 -0500

    Actividad 1 Pasar de libras a Kilos

---
+++ Actividad_semana1.py
@@ -0,0 +1,7 @@
+"""Diseñar un algoritmo que lea el peso de una persona en libras y devuelva su peso en kilogramo.
+Considera que
+1 libra equivale a 0.453592 kilogramos."""
+
+libras = int(input("Ingrese su peso en Libras " )) 
+kilogramos = libras * 0.453592
+print(f"El peso en Kilogramos es: {kilogramos}") 
--- Juego_Snake.py
+++ Juego_Snake.py
@@ -1,93 +1,117 @@
-from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
-
-from sqlalchemy import false
-import pygame
+import pygame:
 import random
 
-class cuerpo:
-    def __init__(self, ventana):
+
+class Cuerpo:
+    def __init__(self, window):
         self.x = 0
         self.y = 0
-        self.dir = 0
-        self.ventana = ventana
-    
-    def dibujar:
-        pygame.draw.rect(self.ventana, (255, 0, 0), (self.x, self.y, 10, 10))    
-    
-    def moverse(selt):
+        self.window = window
+        self.dir = 0  # 0 right, 1 left, 2 down, 3 up
+
+    def draw(self):
+        pygame.draw.rect(self.window, (255, 255, 255), (self.x, self.y, 10, 10))
+
+    def movement(self):
         if self.dir == 0:
-            self.x += 100
+            self.x += 10
         elif self.dir == 1:
-            self.x -= 100
+            self.x -= 10
         elif self.dir == 2:
             self.y += 10
         elif self.dir == 3:
             self.y -= 10
-            
 
-class manzanas:
-    def __init__(self, ventana):
+
+class food:
+    def __init__(self, window):
         self.x = random.randrange(40) * 10
         self.y = random.randrange(40) * 10
-        self.ventana = ventana
-    
-    def dibujar():  
-        pygame.draw.rect(self.ventana, (255, 255, 255), (self.x, self.y, 10, 10))    
-    
-    def nueva manzana(self):
+        self.window = window
+
+    def draw(self):
+        pygame.draw.rect(self.window, (255, 0, 0), (self.x, self.y, 10, 10))
+
+    def relocate(self):
         self.x = random.randrange(40) * 10
         self.y = random.randrange(40) * 10
-       
-def refrescar(ventana):
-    ventana.fill((0, 0, 0))
-    comida.dibujar()
-    for i in range(len(serpiente))
-    serpiente[1].dibujar()
 
-def seguir_cabeza():
-    for i in range(len(serpiente) - 1):
-        serpiente[len(serpeinte) -1 -1].x = serpiente[len(serpiente) - 1 - 2].x
-        serpiente[len(serpeinte) -1 -1].y = serpiente[len(serpiente) - 1 - 2].y    
 
-def main ():
-    global serpiente, comidade
-    ventana = pygame.display.set_mode((400, 400))
-    ventana.fill((0, 0, 0))
-    comida = manzanas(ventana)
-    serpiente = [cuerpo(ventana)]
+def redraw(window):
+    window.fill((0, 0, 0))
+    comida.draw()
+    for i in range(len(snake)):
+        snake[i].draw()
+
+
+def snake_ubicacion():
+    if(len(snake)) > 1:
+        for i in range(len(snake)-1):
+            snake[len(snake)-i-1].x = snake[len(snake)-i-2].x
+            snake[len(snake) - i - 1].y = snake[len(snake) - i - 2].y
+
+
+def Colision():
+    hit = False
+    if (len(snake)) > 1:
+        for i in range(len(snake) - 1):
+            if snake[0].x == snake[i + 1].x and snake[0].y == snake[i + 1].y:
+                hit = True
+    return hit
+
+
+def main():
+    global comida, snake
+    window = pygame.display.set_mode((400, 400))
+    window.fill((0, 0, 0))
+    pygame.display.set_caption("Snake")
+    snake = [Cuerpo(window)]
+    snake[0].draw()
+    comida = food(window)
+    redraw(window)
     run = True
+    velocidad = 100
     while run:
-        for even in pygame.event.get():
-            if event.type == pygame.quit:
-                run = false
-            if event.type == pygame.KEYDOWN
-                if event.key == pygame.K_RIGHT:
-                    serpiente[0].dir = 0
-                if event.key == pygame.K_LEFT:
-                    serpiente[0].dir = 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
-                    serpiente[0].dir = 2
+                    snake[0].dir = 2
+                if event.key == pygame.K_LEFT:
+                    snake[0].dir = 1
+                if event.key == pygame.K_RIGHT:
+                    snake[0].dir = 0
                 if event.key == pygame.K_UP:
-                    serpiente[0].dir = 3
-                
-        serpiente[0].moverse()
-        refrescar(ventana)
+                    snake[0].dir = 3
+        snake_ubicacion()
+        snake[0].movement()
+        redraw(window)
         pygame.display.update()
-        pygame.time.delay(100)
-        if serpiente[0].x == comida.x and serpiente[0].y == comida.y:
-            comida.nueva_manzana()
-            serpiente.append(cuerpo(ventana))
-        seguir_cabeza()
-        if serpiente[0].x >= 400:
-            serpiente[0].x = 0
-        elif serpiente[0].x < 0:
-            serpiente[0].x = 390
-        if serpiente[0].y >= 400:
-            serpiente[0].y = 0
-        elif serpiente[0].y < 0:
-            serpiente[0].y = 390
+        pygame.time.delay(velocidad)
 
-if   __name__ == "__main__":
-    main()
-    pygame.quit()
-                
+        if snake[0].x >= 400:
+            snake[0].x = 0
+        elif snake[0].x < 0:
+            snake[0].x = 390
+
+        if snake[0].y >= 400:
+            snake[0].y = 0
+        elif snake[0].y < 0:
+            snake[0].y = 390
+
+        if Colision():
+            snake = [Cuerpo(window)]
+            comida.relocate()
+            velocidad = 100
+
+        if snake[0].x == comida.x and snake[0].y == comida.y:
+            if velocidad > 35:
+                velocidad -= 5
+            comida.relocate()
+            snake.append(Cuerpo(window))
+            snake_ubicacion()
+
+
+main()
+pygame.quit()
